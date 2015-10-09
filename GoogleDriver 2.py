b8413547926d62f74d90f660ff391d7ad93c43fd
Matt Widjaja <matthew@wimagine.org>
2015-10-08 22:55:46 -0400

    State overlays work!
    State overlays work, what a shocker, I just have to now shade them
    correctly

--- GoogleDriver 2.py
+++ GoogleDriver 2.py
@@ -8,7 +8,7 @@ Created on Sun Oct  4 20:51:35 2015
 from __future__ import print_function
 
 # Imports Google Maps stuff
-from bokeh.models.glyphs import Circle, Patch
+from bokeh.models.glyphs import Circle
 from bokeh.models import (
     GMapPlot, Range1d, ColumnDataSource,
     PanTool, WheelZoomTool, BoxSelectTool,
@@ -23,35 +23,32 @@ import pandas as pd
 # Imports other inhouse functions
 import GunData as gdt
 import LawData as ldt
+import PlotData as pdt
+
+x_range = Range1d()
+y_range = Range1d()
 
 # Creates Output File
 output_file("maps.html", title="Number of People Shot")
 
 # Sets Google Map for America in Bokeh
-x_range = Range1d()
-y_range = Range1d()
 map_options = GMapOptions(lat=39.50, lng=-98.35, map_type="roadmap", zoom=4)
 plot = GMapPlot(x_range=x_range, y_range=y_range, map_options=map_options, \
                 plot_width=1100, plot_height=650, title="United States")
 
-
-""" main: Creates plot for shootings per county -----------------------------"""
 # Loads DataFrame from DownloadGoogle with City, Shootings, Lat, & Long.
 inFile = '/Users/Matthew/Github/Crime2015/MyData/ShootingData2.csv'
 inData = pd.read_csv(inFile, index_col='City')
 
-# Groups all entries by counties
-inData = inData.groupby(['Cty']).sum()
-
 # Set Color Options
-attkColors = {5:'#CCE0FF', 10:'#99C2FF', 20:'#66A3FF', 30:'#3385FF', \
-              40:'#0066FF', 50:'#0052CC', 60:'#003D99', 70:'#002966', \
-              85:'#001433', 100:'#000A1A', 300:'#000000'}
+attkColors = {5:'#CCE0FF', 10:'#99C2FF', 15:'#66A3FF', 20:'#3385FF', \
+              25:'#0066FF', 30:'#0052CC', 40:'#003D99', 50:'#002966', \
+              60:'#001433', 70:'#000A1A', 200:'#000000'}
 clrs = []
 
-# Sets Color for each County
+# Gets Coordinates & Color for each City
 for location in inData.index:
-    shootings = inData.loc[location, 'Shooting']
+    shootings = inData.loc[location, 'Shootings']
     color = '#CCE0FF'
     for maximum in sorted(attkColors):
         if shootings <= maximum:
@@ -61,36 +58,18 @@ for location in inData.index:
             color = attkColors[maximum]
 
 # Sets Data for Shots 
-source = ColumnDataSource(data=dict(lat=inData.Lat, lng=inData.Lng, clr=clrs))
+source = ColumnDataSource(
+    data=dict(
+        lat=inData.Lat,
+        lon=inData.Lng,
+        clr=clrs
+    )
+)
 
-# Create Circles on Plot
-circle = Circle(x="lng", y="lat", size=15, fill_color="clr", line_color="black")
+# Create Circle Plot
+circle = Circle(x="lon", y="lat", size=15, fill_color="clr", line_color="black")
 plot.add_glyph(source, circle)
 
-
-""" main: Creates plot for shootings per county -----------------------------"""
-# Gets State Coordinates
-stateBorder = gdt.loadBorder()
-lats = [stateBorder[item]['lat'] for item in stateBorder]
-lngs = [stateBorder[item]['lng'] for item in stateBorder]
-
-# Sets Color List
-clrs = []
-
-# Sets Law/County for each County
-for county in stateBorder:
-    ldt.
-clrs = ['#CCE0FF' for item in stateBorder]
-
-# Sets Data for each County 
-source = ColumnDataSource(data=dict(lat=lats, lng=lngs, clr=clrs))
-
-# Creates Patches on Plot
-patch = Patch(x="lng", y="lat", fill_alpha=0.5, fill_color="clr", line_width=2)
-plot.add_glyph(source, patch)
-
-
-""" main: Renders & Saves Plot ----------------------------------------------"""
 # Create Tools
 pan = PanTool()
 wheel_zoom = WheelZoomTool()
